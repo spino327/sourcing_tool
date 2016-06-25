0c3965209de0ddfcf18d0028a2ae7eeff4ee58ae
Sergio Pino <spino327@gmail.com>
2016-06-24 23:17:59 -0400

    minor fix

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -5,7 +5,7 @@
 export SOURCING_TOOL_NAME=$BASH_SOURCE
 
 echo "Hello Master! this is $SOURCING_TOOL_NAME."
-echo "Please run '> \$SOURCING_TOOL' to select your prefered env."
+echo "Please run '> \$SOURCING_TOOL' to select your preferred env."
 
 # sourcing_tool
 export SOURCING_TOOL="source $SOURCING_TOOL_HOME/sourcing_tool.sh"
