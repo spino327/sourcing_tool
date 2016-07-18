bd2128d8afd9f15153e2921a1a2718bf3d1366c2
Sergio Pino <spino327@gmail.com>
2016-07-18 10:26:24 -0400

    modification to sourcing_tool.sh to simplify the user's env selection

--- dev/sourcing_tool.sh
+++ dev/sourcing_tool.sh
@@ -7,23 +7,34 @@ echo "*************************"
 echo "OPTIONS:"
 echo "*************************"
 
+
+ITH=0
+OPTIONS=()
 for file in `ls $SOURCING_TOOL_HOME`; do
     if [ "$file" != "new_env.sh" -a "$file" != "sourcing_tool.sh" -a "$file" != "sourcing_load_dependency.sh" -a "$file" != "bootstrap.sh" ]; then
-        echo $file
+        echo "$ITH : $file"
+        OPTIONS[$((ITH++))]=$file
     fi
 done
 echo "*************************"
-echo -n "Please select env. Default [$default]: "
+echo -n "Please select the number for the env. Default [$default]: "
 read pref_env
 
-if [ -z "$pref_env" ]; then
+# default
+if [ -z "$pref_env" -o "$pref_env" -lt "0" -o "$pref_env" -ge "${#OPTIONS[@]}" ]; then
     echo "Using default: "$default
     pref_env=$default
-fi
-if [ -a $SOURCING_TOOL_HOME/$pref_env ]; then
-    echo Sourcing $SOURCING_TOOL_HOME/$pref_env
     export SOURCING_TOOL_NAME="echo $pref_env"
     source $SOURCING_TOOL_HOME/$pref_env
+
 else
-    echo "$pref_env is not a valid env file. try again"
+    # loading selection
+    pref_env=${OPTIONS[$pref_env]}
+    if [ -a $SOURCING_TOOL_HOME/$pref_env ]; then
+        echo Sourcing $SOURCING_TOOL_HOME/$pref_env
+        export SOURCING_TOOL_NAME="echo $pref_env"
+        source $SOURCING_TOOL_HOME/$pref_env
+    else
+        echo "$pref_env is not a valid env file. try again"
+    fi
 fi
